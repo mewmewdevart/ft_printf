3f21b759f5f97e33862bd10c881b15e964776aa8
Meowbutu <mewmewdevart@gmail.com>
2022-11-13 17:22:26 -0300

    more changes

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -17,15 +17,15 @@ static int	ft_translate(va_list arg, char character);
 
 int	ft_printf(const char *str, ...)
 {
-	int	index;
-	int	type_return;
+	int		index;
+	int		type_return;
 	va_list	arg;
 
 	index = 0;
 	type_return = 0;
 	va_start(arg, str);
-	if(!str)
-		return(0);
+	if (!str)
+		return (0);
 	while (str[index] != '\0')
 	{
 		if (str[index] != '%')
@@ -50,18 +50,18 @@ static int	ft_translate(va_list arg, char character)
 
 	type_return = 0;
 	if (character == 's')
-		type_return = ft_putstr(va_arg(arg, char *)); //%s prints a string (as defined by the common C convention
-	else if(character == 'p')
-		type_return = ft_putptr(va_arg(arg, unsigned long int)); //the void * pointer argument has to be printed in hexadecimal format."
+		type_return = ft_putstr(va_arg(arg, char *));
+	else if (character == 'p')
+		type_return = ft_putptr(va_arg(arg, unsigned long int));
 	else if (character == 'd' || character == 'i')
-		type_return = ft_putnbr(va_arg(arg, unsigned long int)); //%d and %i prints a decimal (base 10) number."
-	else if (character == 'u') //%u prints an unsigned decimal (base 10) number"
+		type_return = ft_putnbr(va_arg(arg, unsigned long int));
+	else if (character == 'u')
 		type_return = ft_putnbr_unsigned(va_arg(arg, unsigned int));
-	else if (character == 'x' || character == 'X') //%x prints a number in hexadecimal (base 16) lowercase and uppercase format."
+	else if (character == 'x' || character == 'X')
 		type_return = ft_puthexa(va_arg(arg, unsigned int), character);
 	else if (character == '%')
-		type_return = ft_putchar('%'); //%% printf percent sign %
+		type_return = ft_putchar('%');
 	else
-		type_return = ft_putchar(va_arg(arg, int)); //%c prints a single character
+		type_return = ft_putchar(va_arg(arg, int));
 	return (type_return);
 }
